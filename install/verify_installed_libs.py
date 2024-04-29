d4c734a08aa5e0cb6ffc703348266ef0eb178f8b
bkellenb <benjamin.kellenberger@yale.edu>
2024-04-28 20:26:52 -0400

    GitHub actions test fix

--- install/verify_installed_libs.py
+++ install/verify_installed_libs.py
@@ -4,13 +4,14 @@
     2024 Benjamin Kellenberger
 '''
 
+from typing import Optional, List
 import argparse
 import importlib
 
 
 
 # required package libraries
-LIBS: tuple= (
+LIBS_REQUIRED: tuple= (
     'bottle',
     'gunicorn',
     'psycopg2',
@@ -23,18 +24,32 @@ LIBS: tuple= (
     'celery',
     'cv2',
     'rasterio',
-    'torch',
-    'detectron2'
+    'torch'
+)
+
+# optional package libraries (for certain models)
+LIBS_OPTIONAL: tuple = (
+    'detectron2',
+    'yolov5'
 )
 
 
 
-def verify_libraries(raise_on_first_error: bool=False) -> None:
+def verify_libraries(verify_optional_libs: bool=True,
+                     raise_on_first_error: bool=False,
+                     libs_ignore: Optional[List[str]]=None) -> None:
     '''
         Attempts to load list of required packages one by one. Prints all libraries to the command
         line that could not be imported properly.
     '''
-    for lib in LIBS:
+    if libs_ignore is None:
+        libs_ignore = []
+    libs = LIBS_REQUIRED
+    if verify_optional_libs:
+        libs += LIBS_OPTIONAL
+    for lib in libs:
+        if lib in libs_ignore:
+            continue
         try:
             importlib.import_module(lib)
         except ModuleNotFoundError as exc:
@@ -46,8 +61,15 @@ def verify_libraries(raise_on_first_error: bool=False) -> None:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Verify import of dependencies')
+    parser.add_argument('--verify-optional', type=int,
+                        default=1,
+                        help='Set to 0 to only verify requried libraries (default: 1).')
     parser.add_argument('--raise-on-error', type=int,
                         default=0,
-                        help='Set to 1 to raise on first import error (default: 0)')
+                        help='Set to 1 to raise on first import error (default: 0).')
+    parser.add_argument('--libs-ignore', type=str, nargs='?', required=False,
+                        help='Libraries to skip checking (e.g., for unit tests).')
     args = parser.parse_args()
-    verify_libraries(bool(args.raise_on_error))
+    verify_libraries(bool(args.verify_optional),
+                     bool(args.raise_on_error),
+                     args.libs_ignore)
