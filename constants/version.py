d996e7787908aaf2ce42996d88532e1770b21593
bkellenb <benjamin.kellenberger@yale.edu>
2024-05-09 09:43:24 -0400

    minimum FileServer version update

--- constants/version.py
+++ constants/version.py
@@ -16,7 +16,7 @@ import datetime
 AIDE_VERSION = '3.0.240509'
 
 # minimum required version for FileServer, due to recent changes
-MIN_FILESERVER_VERSION = '3.0.230109c'
+MIN_FILESERVER_VERSION = '3.0.240509'
 
 # model marketplace format version exported by the current AIDE implementation
 MODEL_MARKETPLACE_VERSION = 1.0
